14f52c2eca4ee3b54b18098aec0ec604086ad380
octaviolan <octavio.olan25@gmail.com>
2022-04-15 15:21:10 -0500

    Header version mobile incluido el menu

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+const header = document.querySelector('.header');
+
+navToggle.addEventListener('click', function() {
+  links.classList.toggle('show-links');
+  header.classList.toggle('changeColor-header');
+})
